993cfc8d872353b388caf6720634bf0d56dc1125
Agustin Jerez <agusxxpunk77@gmail.com>
2023-01-30 11:02:15 -0300

    Cv maquetado reponsive en pc y Js

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 function inicializarEventos() {
-    var facebook = document.querySelector("#facebook");
+    var twitter = document.querySelector("#twitter");
     var instagram = document.querySelector("#instagram");
 
     // Agregar eventos de clic a los elementos
-    facebook.addEventListener("click", function() {
-        window.location.href = "https://www.facebook.com/agustin.jerez.37";
+    twitter.addEventListener("click", function() {
+        window.location.href = "https://twitter.com/Agusxpunk";
     });
 
     instagram.addEventListener("click", function() {
         window.location.href = "https://www.instagram.com/agustinpunks";
     });
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    const form = document.getElementById('formulario-contacto');
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const subject = document.getElementById('subject').value;
+        console.log(name, email, subject);
+    });
+});
